2a990ac0a93231ada24ecdbad7165c79dec6e687
Ron Schliep <schliepron24@gmail.com>
2021-05-28 20:35:32 +0200

    aktueller stand rsc

--- foo.js
+++ foo.js
@@ -4,6 +4,11 @@ const eingrenzen = (min, max, x) => {
     return Math.min(max, Math.max(min, x))
 }
 
+const susceptibleColor = "#3076b3";
+const infectedColor = "#b33030";
+const recoveredColor = "#36ad38";
+const deadColor = "#04090d";
+
 // generiert 2 normalverteile zufallszahlen
 const box_muller = () => {
     const u1 = Math.random()
@@ -25,8 +30,9 @@ class Blobmensch {
         this.z2 = 0
         this.normal = true;
         this.infected = false;
-        this.removed = false;
-        this.color = "#123456";
+        this.recovered = false;
+        this.dead = false;
+        this.daysInfected = 0;
     }
     /*
       step(dt) {
@@ -47,12 +53,13 @@ class Blobmensch {
             this.z1 = z1;
             this.z2 = z2;
         }
+        recoverOrDie(this);
 
-        this.vx = 0.15 * this.z1 * dt
-        this.vy = 0.15 * this.z2 * dt
+        this.vx = 0.055 * this.z1 * dt
+        this.vy = 0.055 * this.z2 * dt
 
-        this.x = eingrenzen(0, 500, this.x + this.vx)
-        this.y = eingrenzen(0, 500, this.y + this.vy)
+        this.x = eingrenzen(0, 1920, this.x + this.vx)
+        this.y = eingrenzen(0, 1080, this.y + this.vy)
 
     }
 }
@@ -65,39 +72,40 @@ const distance_between = (x1, y1, x2, y2) => {
 }
 
 const touched = (blob, blobs) => {
-    if (blob.x < 1 || blob.x > 499) {
+    if (blob.x < 1 || blob.x > 1919) {
         return true;
     }
-    if (blob.y < 1 || blob.y > 499) {
+    if (blob.y < 1 || blob.y > 1079) {
         return true;
     }
     for (blob2 of blobs) {
         if (blob === blob2) {
             continue;
         }
-        if (distance_between(blob.x, blob.y, blob2.x, blob2.y) < 0.2) {
-            if (true) {
-                blob.color = "#b33030"
-                blob.infected = true
+        if (distance_between(blob.x, blob.y, blob2.x, blob2.y) < 0.5) {
+            if (blob2.infected && infect(blob)) {
+                blob.normal = false;
+                blob.infected = true;
+            } else if (blob.infected && infect(blob2)) {
+                blob2.normal = false;
+                blob2.infected = true;
             }
             return true;
         }
     }
     return false;
 }
-
-
 const render = (blobs) => {
     const canvas = byId("c")
     const ctx = canvas.getContext("2d")
 
     ctx.fillStyle = "#ffffff"
-    ctx.fillRect(-1, -1, 502, 502)
+    ctx.fillRect(-1, -1, 1921, 1081)
 
     for (blob of blobs) {
-        ctx.fillStyle = blob.color
+        ctx.fillStyle = getColor(blob)
         ctx.beginPath()
-        ctx.arc(blob.x, blob.y, 5, 0, 2 * Math.PI)
+        ctx.arc(blob.x, blob.y, 6, 0, 2 * Math.PI)
         ctx.fill()
     }
 }
@@ -119,10 +127,49 @@ const step = (t) => {
     window.requestAnimationFrame(step)
 }
 
+const recoverOrDie = (blob) => {
+    if(blob.recovered || blob.dead) return;
+
+    //recover
+    if (blob.infected && blob.daysInfected < 400) {
+        blob.daysInfected++;
+    } else if (blob.infected) {
+        blob.infected = false;
+        blob.recovered = true;
+    }
+
+    //die
+    const u1 = Math.random() * 100;
+    if(blob.infected && u1 <= 0.01){
+        blob.infected = false;
+        blob.dead = true;
+    }
+
+}
+
+const infect = (blob) => {
+    if(!blob.normal) return false
+    const u1 = Math.random();
+    return u1 <= 0.1
+}
+
+
+const getColor = (blob) => {
+    if (blob.infected) return infectedColor
+    else if (blob.dead) return deadColor
+    else if (blob.recovered) return recoveredColor
+    else return susceptibleColor
+}
+
 const blobs = []
-for (let i = 0; i < 100; i++) {
-    const b = new Blobmensch(Math.random() * 500, Math.random() * 500)
+for (let i = 0; i < 499; i++) {
+    const b = new Blobmensch(Math.random() * 1920, Math.random() * 1080)
     blobs.push(b)
 }
+
+const b1 = new Blobmensch(Math.random() * 1920, Math.random() * 1080)
+b1.color = infectedColor
+b1.infected = true
+blobs.push(b1)
 
 window.requestAnimationFrame(step)
