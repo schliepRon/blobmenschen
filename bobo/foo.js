19b74622b77c4816813b1a9fec4022c80ab357a7
Carsten Brachem <carsten.brachem@gmail.com>
2021-05-29 10:39:49 +0200

    Doppelte StartSim-Methode entfernt

--- bobo/foo.js
+++ bobo/foo.js
@@ -5,109 +5,70 @@ import { byId } from "./util.js"
 
 const blobs = [];
 
-const startSim = (id) => {
-    const WIDTH = 700
+const startSim = (id, empty = false, color = "#ffffff") => {
+  const WIDTH = 700
 
-    const params = Params.fromSliders()
-    params.width = WIDTH;
-    params.logValues()
+  const params = Params.fromSliders()
+  params.width = WIDTH;
+  
+  if (empty) {
+    params.blobCount = 0;
+  }
+  
+  params.logValues()
 
 
-    const render = (blobs) => {
-      const canvas = byId(id)
-      const ctx = canvas.getContext("2d")
+  const render = (blobs) => {
+    const canvas = byId(id)
+    const ctx = canvas.getContext("2d")
 
-      ctx.fillStyle = "#ffffff"
-      ctx.fillRect(-1, -1, params.width + 2, params.width + 2)
+    ctx.fillStyle = color
+    ctx.fillRect(-1, -1, params.width + 2, params.width + 2)
 
-      for (const blob of blobs) {
-        blob.draw(ctx)
-      }
+    for (const blob of blobs) {
+      blob.draw(ctx)
     }
+  }
 
-    let t0 = null
+  let t0 = null
 
-    const step = (t) => {
-      if (!t0) {
-        t0 = t
+  const step = (t) => {
+    if (!t0) {
+      t0 = t
+      if (!empty) {
         blobs[0].state = "infected"
         blobs[0].infectedAt = t
         blobs[0].distincing = false
       }
+    }
 
-      const dt = 0.9 * (t - t0)
-      if (dt <= 0) {
-        window.requestAnimationFrame(step)
-        return
-      }
-      t0 = t
-
-      const myColonyBlobs = blobs.filter(e => e.currentColony == id)
-
-      for (const blob of myColonyBlobs) {
-        blob.step(t, 20, myColonyBlobs)
-      }
-
-      render(myColonyBlobs)
-
+    const dt = 0.9 * (t - t0)
+    if (dt <= 0) {
       window.requestAnimationFrame(step)
+      return
+    }
+    t0 = t
+
+    const myColonyBlobs = blobs.filter(e => e.currentColony == id)
+
+    for (const blob of myColonyBlobs) {
+      blob.step(t, 20, myColonyBlobs)
     }
 
-    for (let i = 0; i < params.blobCount; i++) {
-      const b = new Blobmensch(Math.random() * (params.width - 100) + 50, Math.random() * (params.width - 100) + 50, id, params)
-      blobs.push(b)
-    }
+    render(myColonyBlobs)
 
     window.requestAnimationFrame(step)
+  }
+
+  for (let i = 0; i < params.blobCount; i++) {
+    const b = new Blobmensch(Math.random() * (params.width - 100) + 50, Math.random() * (params.width - 100) + 50, id, params)
+    blobs.push(b)
+  }
+
+  window.requestAnimationFrame(step)
 }
 
-const startSimEmpty = (id) => {
-    const byId = id => document.getElementById(id)
-    const WIDTH = 700
-
-    const params = Params.fromSliders()
-    params.width = WIDTH
-    params.logValues()
-
-    const render = (blobs) => {
-      const canvas = byId(id)
-      const ctx = canvas.getContext("2d")
-
-      ctx.fillStyle = "#dddddd"
-      ctx.fillRect(-1, -1, params.width + 2, params.width + 2)
-
-      for (const blob of blobs) {
-        blob.draw(ctx)
-      }
-    }
-
-    let t0 = null
-
-    const step = (t) => {
-      if (!t0) {
-        t0 = t
-      }
-
-      const dt = 0.9 * (t - t0)
-      if (dt <= 0) {
-        window.requestAnimationFrame(step)
-        return
-      }
-      t0 = t
-
-      const myColonyBlobs = blobs.filter(e => e.currentColony == id)
-
-      for (const blob of myColonyBlobs) {
-        blob.step(t, 20, myColonyBlobs)
-      }
-
-      render(myColonyBlobs)
-
-      window.requestAnimationFrame(step)
-    }
-
-    window.requestAnimationFrame(step)
-}
+const startSimEmpty = (id) => startSim(id, true, "#cccccc")
 
 const startPlot = () => {
     const WIDTH = 700
